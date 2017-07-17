595e9c0da5c360c96b55985dc46c1d27d39551ba
Adam Ivora <adam.ivora@tieto.com>
2017-07-17 09:49:02 +0200

    fix test project

---
+++ test/GraphQLCoreExperimental.Tests/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace GraphQLCoreExperimental.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+        }
+    }
+}
